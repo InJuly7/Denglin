c87e425dadfdd2bb21a9511944078b0ce90d05b3
Tingshuo <Songge200007@163.com>
2025-05-11 13:42:13 +0000

    update model path

--- main.cpp
+++ main.cpp
@@ -87,8 +87,8 @@ int main(int argc ,char *argv[]) {
     utils::InitParameter param;
     setParameters(param);
 
-    std::string model_path = "/home/firefly/Tingshuo/yolov5s/yolov5s_7sim.quantized.rlym";
-    std::string engine_path = "/home/firefly/Tingshuo/yolov5s/yolov5s_7sim.quantized.slz";
+    std::string model_path = "/home/firefly/Tingshuo/yolov5s/yolov5s_7sim.rlym";
+    std::string engine_path = "/home/firefly/Tingshuo/yolov5s/yolov5s_7sim.slz";
     std::string video_path = "/home/firefly/Tingshuo/yolov5s/vedio.mp4";
     std::string image_path = "/home/firefly/Tingshuo/yolov5s/640640.jpg";
     int camera_id = 0;
